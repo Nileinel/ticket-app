d5c1e15c8b2d53a059804908736405a21c5ff7f2
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 00:50:16 +0100

    Create tickets-list component template

---
+++ client/src/app/tickets-list/tickets-list.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-tickets-list',
+  imports: [],
+  template: `
+    <p>
+      tickets-list works!
+    </p>
+  `,
+  styles: ``
+})
+export class TicketsList {
+
+}
