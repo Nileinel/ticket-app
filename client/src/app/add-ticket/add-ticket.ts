3dbbcaa3f3d2f6702e19f465e3cc6860f1e2ba9c
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 02:08:11 +0100

    Initialise add ticket component

---
+++ client/src/app/add-ticket/add-ticket.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-add-ticket',
+  imports: [],
+  template: `
+    <p>
+      add-ticket works!
+    </p>
+  `,
+  styles: ``
+})
+export class AddTicket {
+
+}
