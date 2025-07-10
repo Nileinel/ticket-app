2c93930192749c637d044c46847dd2930e48b81b
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 01:30:50 +0100

    Add TicketsList component to entrypoint

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { TicketsList } from './tickets-list/tickets-list';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: TicketsList, title: 'Tickets List' },
+];
--- client/src/app/app.ts
+++ client/src/app/app.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { TicketsList } from './tickets-list/tickets-list';
+import { MatToolbarModule } from '@angular/material/toolbar';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
-  template: `
-    <h1>Welcome to {{title}}!</h1>
-
-    <router-outlet />
+  standalone: true,
+  imports: [RouterOutlet, TicketsList, MatToolbarModule],
+  styles: [
+    `
+      main {
+        display: flex; 
+        justify-content: center;
+        padding: 2rem 4rem;
+      }
+    `,
+  ],
+  template: 
+  `
+    <mat-toolbar color="primary">
+      <span>Ticket Management App</span>
+    </mat-toolbar>
+    <main>
+      <router-outlet></router-outlet>
+    </main>
   `,
-  styles: [],
 })
 export class App {
   protected title = 'client';
