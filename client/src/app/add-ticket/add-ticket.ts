a06fe21879155661c251e90b0cb28b30846956ca
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 02:12:30 +0100

    Implement add ticket component

--- client/src/app/add-ticket/add-ticket.ts
+++ client/src/app/add-ticket/add-ticket.ts
@@ -1,15 +1,42 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { TicketForm } from '../ticket-form/ticket-form';
+import { Ticket } from '../ticket';
+import { TicketService } from '../ticket.service';
+import { MatCardModule } from '@angular/material/card';
+
 
 @Component({
   selector: 'app-add-ticket',
-  imports: [],
+  standalone: true, 
+  imports: [MatCardModule, TicketForm],
   template: `
-    <p>
-      add-ticket works!
-    </p>
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>Add New Ticket</mat-card-title>
+      </mat-card-header>
+      <mat-card-content>
+        <app-ticket-form (formSubmitted)="addTicket($event)"></app-ticket-form>
+      </mat-card-content>
+    </mat-card>
   `,
   styles: ``
 })
 export class AddTicket {
+  constructor(private ticketService: TicketService, private router: Router) {}
+
+  addTicket(ticket: Ticket) {
+    this.ticketService.createTicket(ticket).subscribe({
+      next: () => {
+        this.router.navigate(['/']);
+      },
+      error: (err) => {
+        alert(('Failed to create ticket'));
+        console.error('Error adding ticket:', err);
+      },
+    });
+
+    this.ticketService.getTickets();
+  }   
 
 }
