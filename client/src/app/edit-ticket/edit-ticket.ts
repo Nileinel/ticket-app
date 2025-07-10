597871fc1cdd2befb7ffe189f8c68fdee585be11
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 02:13:08 +0100

    Initialise edit ticket component

---
+++ client/src/app/edit-ticket/edit-ticket.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-edit-ticket',
+  imports: [],
+  template: `
+    <p>
+      edit-ticket works!
+    </p>
+  `,
+  styles: ``
+})
+export class EditTicket {
+
+}
