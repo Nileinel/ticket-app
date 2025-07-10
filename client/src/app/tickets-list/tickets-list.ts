99c7cb2c502df14eb275ac6f0f9543f0ca3df2a7
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 01:12:40 +0100

    Add TicketsList component

--- client/src/app/tickets-list/tickets-list.ts
+++ client/src/app/tickets-list/tickets-list.ts
@@ -1,15 +1,87 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, WritableSignal } from '@angular/core';
+import { Ticket } from '../ticket'; 
+import { TicketService } from '../ticket.service';
+import { RouterModule} from '@angular/router';
+import { MatTableModule } from '@angular/material/table';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-tickets-list',
-  imports: [],
-  template: `
-    <p>
-      tickets-list works!
-    </p>
+  imports: [RouterModule, MatTableModule, MatButtonModule, MatCardModule],
+  styles: [
+    `
+      table {
+        width: 100%;
+        button:first-of-type{
+          margin-right: 1rem;
+        }
+      }
+    `,
+  ],
+  template:`
+    <mat-card>
+      <mat-card-header>
+        <mat-card-title>Tickets List</mat-card-title>
+      </mat-card-header>
+      <mat-card-content>
+        <table mat-table [dataSource]="tickets$()">
+          <ng-container matColumnDef="col-id">
+            <th mat-header-cell *matHeaderCellDef> ID </th>
+            <td mat-cell *matCellDef="let ticket"> {{ticket.id}} </td>
+          </ng-container>
+
+          <ng-container matColumnDef="col-name">
+            <th mat-header-cell *matHeaderCellDef> Name </th>
+            <td mat-cell *matCellDef="let ticket"> {{ticket.name}} </td>
+          </ng-container>
+
+          <ng-container matColumnDef="col-actions">
+            <th mat-header-cell *matHeaderCellDef> Actions </th>
+            <td mat-cell *matCellDef="let ticket">
+              <button mat-raised-button [routerLink] = "['edit/', ticket._id]"> Edit </button>
+              <button mat-raised-button color="warn" (click)="deleteTicket(ticket._id || '')"> Delete </button>
+            </td>
+          </ng-container>
+
+          <tr mat-header-row *matHeaderRowDef="displayedColumns"></tr>
+          <tr mat-row *matRowDef="let row; columns: displayedColumns;"></tr>
+        </table>
+      </mat-card-content>
+      <mat-card-actions>
+        <button mat-raised-button color="primary" [routerLink]="['new']">Create Ticket</button>
+      </mat-card-actions>
+    </mat-card>
   `,
-  styles: ``
 })
-export class TicketsList {
+
+export class TicketsList implements OnInit {
+  tickets$ = {} as WritableSignal<Ticket[]>;
+  displayedColumns: string[] = [
+    'col-id',
+    'col-name',
+    'col-actions'
+  ];
+
+  constructor(private ticketService: TicketService){}
+
+  ngOnInit(){
+    this.fetchTickets();
+  }
+
+  deleteTicket(id: string): void {
+    this.ticketService.deleteTicket(id).subscribe(({
+      next: () => {
+        console.log(`Ticket with ID ${id} deleted successfully.`);
+        this.fetchTickets();
+      }
+    }));
+  }
+
+  private fetchTickets(): void {
+    this.tickets$ = this.ticketService.tickets$;
+    this.ticketService.getTickets();
+  }
 
 }
+
