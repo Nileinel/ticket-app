de1a53f831991cea0d87de3b33b3fdb1ec29d8e9
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 02:32:18 +0100

    Add routing between add-edit-list

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -1,6 +1,10 @@
 import { Routes } from '@angular/router';
 import { TicketsList } from './tickets-list/tickets-list';
+import { AddTicket } from './add-ticket/add-ticket';
+import { EditTicket } from './edit-ticket/edit-ticket';
 
 export const routes: Routes = [
   { path: '', component: TicketsList, title: 'Tickets List' },
+  { path: 'new', component: AddTicket },
+  { path: 'edit/:id', component: EditTicket, title: 'Edit Ticket' },
 ];
