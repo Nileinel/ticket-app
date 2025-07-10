c63cdf3b4a755aa281553a5842593f6a91d824b0
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 01:41:45 +0100

    Add ticket create form template

---
+++ client/src/app/ticket-form/ticket-form.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ticket-form',
+  imports: [],
+  template: `
+    <p>
+      ticket-form works!
+    </p>
+  `,
+  styles: ``
+})
+export class TicketForm {
+
+}
