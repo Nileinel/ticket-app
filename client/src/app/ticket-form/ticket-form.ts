ba1669857d330b06184b37b270cbf3fa44cce098
Nile Inel <nilinel2002@hotmail.com>
2025-07-10 02:07:26 +0100

    Implement create ticket form

--- client/src/app/ticket-form/ticket-form.ts
+++ client/src/app/ticket-form/ticket-form.ts
@@ -1,15 +1,80 @@
-import { Component } from '@angular/core';
+import { Component, effect, EventEmitter, input, Output } from '@angular/core';
+import { FormBuilder, Validators, ReactiveFormsModule, FormGroup } from '@angular/forms';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { Ticket } from '../ticket';
 
 @Component({
   selector: 'app-ticket-form',
-  imports: [],
-  template: `
-    <p>
-      ticket-form works!
-    </p>
+  standalone: true,
+  imports: [
+    ReactiveFormsModule, 
+    MatInputModule, 
+    MatFormFieldModule,
+  ],
+  styles: `
+    .ticket-form {
+      display: flex;
+      flex-direction: column;
+      align-items: flex-start;
+      padding: 2rem;
+    }
+    .mat-mdc-form-field {
+      width: 100%;
+    }
   `,
-  styles: ``
+  template: `
+    <form class="ticket-form" autocomplete="off" [formGroup]="ticketForm" (submit)="submitForm()">
+      <mat-form-field>
+        <mat-label>Name</mat-label>
+        <input matInput placeholder="Enter ticket name" formControlName="name" required />
+        <mat-error *ngIf="name.invalid"> Name must be at least 3 characters long. </mat-error>
+      </mat-form-field>
+      <br />
+      <button 
+        mat-raised-button 
+        color="primary" 
+        type="submit" 
+        [disabled]="ticketForm.invalid"
+      >
+        Add Ticket
+      </button>
+    </form>
+  `,
 })
 export class TicketForm {
+  initialState = input<Ticket>();
+
+  @Output() formValuesChanged = new EventEmitter<Ticket>();
+
+  @Output() formSubmitted = new EventEmitter<Ticket>();
+
+  ticketForm: FormGroup;
+
+  constructor(private formBuilder: FormBuilder) {
+    this.ticketForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.minLength(3)]],
+    });
+
+    effect(() => {
+      this.ticketForm.setValue({
+        name: this.initialState()?.name || '',
+      })
+    });
+
+    this.ticketForm.valueChanges.subscribe((value) => {
+      this.formValuesChanged.emit(value as Ticket);
+    });
+  }
+
+  get name(){
+    return this.ticketForm.get('name')!;
+  }
+
+  submitForm() {
+    if (this.ticketForm.valid) {
+      this.formSubmitted.emit(this.ticketForm.value as Ticket);
+    }
+  }
 
 }
